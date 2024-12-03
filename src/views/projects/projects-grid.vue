bf47ba638f2145ac2e64959170ae10d3207f8dfa
Naufal Alif vivaldi <alifvivaldi726@gmail.com>
2024-12-03 16:17:55 +0700

    Laporan Project pokok jadi

--- src/views/projects/projects-grid.vue
+++ src/views/projects/projects-grid.vue
@@ -1,94 +1,113 @@
 <script>
 import Layout from "../../layouts/main";
 import PageHeader from "@/components/page-header";
-import Pagination from "@/components/common/pagination"
-import { projectData } from "./data-projects";
+// import Sidebar from "../../components/side-bar.vue";
+import Page from "../../components/common/pagination.vue";
+
 
 /**
- * Projects-grid component
+ * Task-list component
  */
 export default {
-  components: {
-    Layout,
-    PageHeader,
-    Pagination
-  },
-  data() {
-    return {
-      projectData: projectData,
-    };
-  },
+  components: { Layout, PageHeader,Page, },
 };
 </script>
 
 <template>
+  <!-- <Sidebar/> -->
   <Layout>
-    <PageHeader title="Projects Grid" pageTitle="Projects" />
-
+    <PageHeader title="Laporan Project" pageTitle="Project" />
     <BRow>
-      <BCol xl="4" sm="6" v-for="grid in projectData" :key="grid.id">
+      <BCol lg="12">
         <BCard no-body>
-          <BCardBody>
-            <div class="d-flex">
-              <div class="avatar-md me-4">
-                <span class="avatar-title rounded-circle bg-light text-danger font-size-16">
-                  <img :src="`${grid.image}`" alt height="30" />
-                </span>
+          <BCardBody class="pb-0">
+            <BCardTitle>Laporan Project</BCardTitle>
+            <!-- <p class="card-title-desc">
+              Create responsive tables by wrapping any <code>.table</code> in
+              <code>.table-responsive</code>
+              to make them scroll horizontally on small devices (under 768px).
+            </p> -->
+            <!-- <div class="col-md-3">
+                <input type="text" class="form-control" id="searchTable" v-on:change="searchTable" v-model="searchDataTable" placeholder="Search Data ..." />
+            </div> -->
+            <form class="row col-12 " style="margin-bottom: 1%;">
+              <div class="col-2">
+                <label class="visually-hidden" for="autoSizingSelect">Preference</label>
+                <select class="form-select" id="autoSizingSelect">
+                  <option selected>Pilih</option>
+                  <option value="1">10</option>
+                  <option value="2">50</option>
+                  <option value="3">100</option>
+                </select>
               </div>
-
-              <div class="flex-grow-1 overflow-hidden">
-                <h5 class="text-truncate font-size-15">
-                  <BLink href="javascript: void(0);" class="text-dark">{{
-                    grid.text
-                  }}</BLink>
-                </h5>
-                <p class="text-muted mb-4">{{ grid.subtext }}</p>
-                <div class="avatar-group">
-                  <div class="avatar-group-item">
-                    <BLink href="javascript: void(0);" class="d-inline-block">
-                      <img :src="`${grid.users[0]}`" class="rounded-circle avatar-xs" alt />
-                    </BLink>
-                  </div>
-                  <div class="avatar-group-item" v-if="grid.users[1]">
-                    <BLink href="javascript: void(0);" class="d-inline-block">
-                      <img :src="`${grid.users[1]}`" class="rounded-circle avatar-xs" alt />
-                    </BLink>
-                  </div>
-                  <div class="avatar-group-item" v-if="grid.users[2]">
-                    <BLink href="javascript: void(0);" class="d-inline-block">
-                      <img :src="`${grid.users[2]}`" class="rounded-circle avatar-xs" alt />
-                    </BLink>
-                  </div>
+              <div class="col-7">
+                <input type="text" class="form-control" id="autoSizingInput" placeholder="Cari">
+              </div>
+              <div class="col-3" style="margin-left: auto;" >
+                <button type="button" class="btn btn-success h-100 w-100" alt="Disable"><i class="mdi-microsoft-excel"></i> EXPORT TO EXCEL</button>
+              </div>
+            </form>
+            
+            <form class="row col-auto" style="margin-bottom: 2%;">
+              <div class="row col-8" style="border: 1px solid black; margin-left: 1%; padding: 10px;">
+                <div class="col-4">
+                  <label class="visually-hidden" for="autoSizingSelect">Preference</label>
+                  <select class="form-select" id="autoSizingSelect">
+                    <option selected>Pilih</option>
+                    <option value="1">10</option>
+                    <option value="2">50</option>
+                    <option value="3">100</option>
+                  </select>
+                </div>
+                <div class="col-4">
+                  <input type="text" class="form-control" id="autoSizingInput" placeholder="Cari">
+                </div>
+                <div class="col-4">
+                  <input type="text" class="form-control" id="autoSizingInput" placeholder="Cari">
                 </div>
               </div>
+              
+              <div class="col-2" style="margin-left: 7%; margin-top: 1%;">
+                <button type="button" class="btn btn-info h-80 w-100" alt="Disable"><i class="bx bx-filter"></i> FILTER</button>
+              </div>
+            </form>
+           
+            <div class="table-responsive">
+              <BTableSimple class="mb-0">
+                <BThead>
+                  <BTr style="border-collapse: collapse; border: 1px solid black">
+                    <BTh style="background-color: #272b4e; color: whitesmoke;text-align: center; vertical-align: middle;border-collapse: collapse; border: 1px solid black;">No</BTh>
+                    <BTh style="background-color: #272b4e; color: whitesmoke;text-align: center; border-collapse: collapse; border: 1px solid black;">Nama Project <button class="btn btn-sm btn-link p-0"><i class="fa fa-sort"></i></button></BTh>
+                    <BTh style="background-color: #272b4e; color: whitesmoke;text-align: center; border-collapse: collapse; border: 1px solid black;">Progress % <button class="btn btn-sm btn-link p-0"><i class="fa fa-sort"></i></button></BTh>
+                    <BTh style="background-color: #272b4e; color: whitesmoke;text-align: center; border-collapse: collapse; border: 1px solid black;">Tanggal Deadline <button class="btn btn-sm btn-link p-0"><i class="fa fa-sort"></i></button></BTh>
+                    <BTh style="background-color: #272b4e; color: whitesmoke;text-align: center; border-collapse: collapse; border: 1px solid black;">Sisa Waktu <button class="btn btn-sm btn-link p-0"><i class="fa fa-sort"></i></button></BTh>
+                    <BTh style="background-color: #272b4e; color: whitesmoke;text-align: center; border-collapse: collapse; border: 1px solid black;">Status Deadline <button class="btn btn-sm btn-link p-0"><i class="fa fa-sort"></i></button></BTh>
+                    <!-- <BTh style="background-color: #272b4e; color: whitesmoke;text-align: center; border-collapse: collapse; border: 1px solid black;vertical-align: middle;" rowspan="2">Aksi</BTh> -->
+                  </BTr>
+                  <!-- <BTr style="border-collapse: collapse; border: 1px solid black;">
+                    <BTh style="background-color: #272b4e; color: whitesmoke; border-collapse: collapse; border: 1px solid black;"><input type="text" placeholder="Search User" class="form-control"></BTh>
+                    <BTh style="background-color: #272b4e; color: whitesmoke; border-collapse: collapse; border: 1px solid black;"><input type="text" placeholder="Search User" class="form-control"></BTh>
+                    <BTh style="background-color: #272b4e; color: whitesmoke; border-collapse: collapse; border: 1px solid black;"><input type="text" placeholder="Search User" class="form-control"></BTh>
+                    <BTh style="background-color: #272b4e; color: whitesmoke; border-collapse: collapse; border: 1px solid black;"><input type="text" placeholder="Search User" class="form-control"></BTh>
+                    <BTh style="background-color: #272b4e; color: whitesmoke; border-collapse: collapse; border: 1px solid black;"><input type="text" placeholder="Search User" class="form-control"></BTh>
+                  </BTr> -->
+                </BThead>
+                <BTbody>
+                  <BTr style="border-collapse: collapse; border: 1px solid black">
+                    <BTh scope="row">1</BTh>
+                    <BTd style="border-collapse: collapse; border: 1px solid black;">Project A</BTd>
+                    <BTd style="border-collapse: collapse; border: 1px solid black;">Project Cukup sulit</BTd>
+                    <BTd style="border-collapse: collapse; border: 1px solid black;">20/11/2021</BTd>
+                    <BTd style="border-collapse: collapse; border: 1px solid black;text-align: center;"><span class="badge bg-danger">3 Years Ago</span></BTd>
+                    <BTd style="border-collapse: collapse; border: 1px solid black;text-align: center;"><span class="badge bg-danger">Terlambat</span></BTd>
+                  </BTr>
+                </BTbody>
+              </BTableSimple>
             </div>
           </BCardBody>
-          <div class="px-4 py-3 border-top">
-            <ul class="list-inline mb-0">
-              <li class="list-inline-item me-3">
-                <span class="badge" :class="{
-                  'bg-success': `${grid.status}` === 'Completed',
-                  'bg-warning': `${grid.status}` === 'Pending',
-                  'bg-danger': `${grid.status}` === 'Delay',
-                }">{{ grid.status }}</span>
-              </li>
-              <li v-b-tooltip.hover.top class="list-inline-item me-3" title="Due Date">
-                <i class="bx bx-calendar me-1"></i>
-                {{ grid.date }}
-              </li>
-              <li v-b-tooltip.hover.top class="list-inline-item me-3" title="Comments">
-                <i class="bx bx-comment-dots me-1"></i>
-                {{ grid.comment }}
-              </li>
-            </ul>
-          </div>
         </BCard>
       </BCol>
     </BRow>
-    <BRow>
-      <BCol lg="12">
-        <Pagination />
-      </BCol>
-    </BRow>
+  <Page/>
   </Layout>
 </template>
